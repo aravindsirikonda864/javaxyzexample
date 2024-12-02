a6db27092e6d7eea9745dc5907824c6fc6370754
aravind <saravindsirikonda@gmail.com>
2024-12-02 15:26:13 +0530

    java docker test

---
+++ xyz.java
@@ -0,0 +1,39 @@
+public class XYZ {
+    public static void main(String[] args) {
+        // Initialize the string
+        String str = "Hello, World!";
+        
+        // Create a StringBuilder object
+        StringBuilder reversedStr = new StringBuilder(str);
+        
+        // Reverse the string
+        reversedStr.reverse();
+        
+        // Output the reversed string
+        System.out.println("Reversed String: " + reversedStr.toString());
+    }
+}
+
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+xyz.java [unix] (05:29 01/01/1970)                                       0,1 All
+-- INSERT --
+
